5f4e6d887de4db6f32226d385e69b34bbfe17b44
ahmed amr <ahmedamro3000@gmail.com>
2023-12-26 01:08:01 +0200

    welcome

---
+++ Js/index.js
@@ -0,0 +1,327 @@
+//    ***********************************************     1     *****************************
+
+// var number =  (window.prompt("enter Number "))
+//  console.log(number);
+
+//    ***********************************************     2    *****************************
+
+// var number = window.prompt("enter Number ");
+
+// if (number % 4 == 0 && number % 3 == 0) {
+//   console.log(" yes");
+// } else {
+//         console.log("no");
+// }
+
+//    ***********************************************     3   *****************************
+
+// var num1 = window.prompt("enter num1 ");
+// var num2 = window.prompt("enter num2 ");
+
+// if (num1 > num2) {
+//   console.log(num1);
+// } else if (num1 < num2) {
+//   console.log(num2);
+// }
+
+//    ***********************************************     4  *****************************
+
+//  var num1 = window.prompt("enter num ");
+
+//  if ( num1 > 0) {
+// console.log(num1 * - 1)
+
+//  } else if (num1 < 0){
+//  console.log(num1 * - 1);
+//  }
+
+//    ***********************************************   5  *****************************
+
+// var num1 = window.prompt("enter num1 ");
+// var num2 = window.prompt("enter num2 ");
+// var num3 = window.prompt("enter num3 ");
+
+// if (num1 > num2 && num1 > num3 && num2 > num3) {
+//   console.log("max element = " + num1 + " min element = " + num3);
+// } else if (num1 > num2 && num1 > num3 && num3 > num2) {
+//   console.log("max element = " + num1 + " min element = " + num2);
+// } else if (num2 > num1 && num2 > num3 && num1 > num3) {
+//   console.log("max element = " + num2 + " min element = " + num3);
+// } else if (num2 > num2 && num2 > num3 && num3 > num1) {
+//   console.log("max element = " + num2 + " min element = " + num1);
+// } else if (num3 > num1 && num3 > num2 && num1 > num2) {
+//   console.log("max element = " + num3 + " min element = " + num2);
+// } else if (num3 > num1 && num3 > num2 && num2 > num1) {
+//   console.log("max element = " + num3 + " min element = " + num1);
+// }
+
+//    ***********************************************   6  *****************************
+
+// var num1 = window.prompt("enter num1 ");
+
+// if (num1 % 2 == 0) {
+// console.log("even");
+
+// } else {
+//         console.log("odd");
+// }
+
+//    ***********************************************   8  *****************************
+
+// var char = window.prompt("enter word ");
+
+// if (char=='a' || char=='e' || char=='i' || char=='o' || char=='u'  || char=='A' || char=='E' || char=='I' || char=='O' || char=='U') {
+//   console.log("vowel");
+// } else {
+//   console.log("Consonant");
+// }
+
+//    ***********************************************   9  *****************************
+
+//  var num = window.prompt("enter num ");
+
+// for ( var i = 1 ; i<= num ; i++) {
+//         console.log(i);
+
+// }
+
+//    ***********************************************   10  *****************************
+
+// var num = window.prompt("enter num ");
+// for (let i = 1; i <= 12; ++i)
+//     console.log(num + " * " + i +
+//         " = " + num * i);
+
+//    ***********************************************   11  *****************************
+
+//  var num = window.prompt("enter num ");
+
+//  for ( var i = 2 ; i<= num ; i+= 2) {
+//       console.log(i);
+
+//  }
+
+//    ***********************************************   12  *****************************
+
+// var num1 = window.prompt("enter num1 ");
+// var num2 = window.prompt("enter num2 ");
+
+// let num = 1;
+
+// for (let i = 0; i < num2; ++i) {
+//   num = num * num1;
+// }
+
+// console.log(num);
+
+//    ***********************************************   12   *****************************
+
+// var num1 = Number(window.prompt("enter num1"));
+// var num2 = Number(window.prompt("enter num2"));
+// var num3 = Number(window.prompt("enter num3"))
+// var num4 = Number(window.prompt("enter num4"))
+// var num5 = Number(window.prompt("enter num5"))
+
+// var total = num1 + num2 + num3 + num4 + num5;
+// var per = (total/500)*100
+
+// console.log("Total is : " + total);
+// console.log("Average Marks = " + total / 5);
+// console.log( "Percentage " + per +" %");
+
+//    ***********************************************   13   *****************************
+
+// var month = Number(window.prompt("enter month"));
+
+// if ((month == 1)) {
+//   console.log("Days in Month: 31");
+// } else if ((month == 2)) {
+//   console.log("Days in Month: 28");
+// } else if ((month == 3)) {
+//   console.log("Days in Month: 31");
+// } else if ((month == 4)) {
+//   console.log("Days in Month: 30");
+// } else if ((month == 5)) {
+//   console.log("Days in Month: 31");
+// } else if ((month == 6)) {
+//   console.log("Days in Month: 30");
+// } else if ((month == 7)) {
+//   console.log("Days in Month: 31");
+// } else if ((month == 8)) {
+//   console.log("Days in Month: 31");
+// } else if ((month == 9)) {
+//   console.log("Days in Month: 30");
+// } else if ((month == 10)) {
+//   console.log("Days in Month: 30");
+// } else if ((month == 11)) {
+//   console.log("Days in Month: 30");
+// } else if ((month == 12)) {
+//   console.log("Days in Month: 31");
+// }
+
+//    ***********************************************   14  *****************************
+
+// var physics = Number(window.prompt("Enter physics "))
+// var chemistry = Number(window.prompt("Enter chemistry "))
+// var biology = Number(window.prompt("Enter biology "))
+// var mathematics = Number(window.prompt("Enter mathematics "))
+// var computer = Number(window.prompt("Enter computer "))
+
+// var total = physics +chemistry + biology + mathematics + computer
+
+// var result1 = total/500*100
+
+// if(result1>=90){
+//     console.log(" 90%: Grad A");
+// }else if(result1>=80){
+//     console.log(" 80%: Grad B");
+// }else if(result1>=70){
+//     console.log(" 70%: Grad C");
+// }else if(result1>=60){
+//     console.log(" 60%: Grad D");
+// }else if(result1>=40){
+//     console.log(" 40%: Grad E");
+// }else{
+//     console.log(" 40%: Grad F");
+// }
+
+//    ***********************************************   15  *****************************
+
+// var month = Number(window.prompt("enter month"));
+
+// switch (month) {
+//   case 1:
+//     console.log("Days in Month: 31");
+//     break;
+//   case 2:
+//     console.log("Days in Month: 28");
+//     break;
+//   case 3:
+//     console.log("Days in Month: 31");
+//     break;
+//   case 4:
+//     console.log("Days in Month: 30");
+//     break;
+//   case 5:
+//     console.log("Days in Month: 31");
+//     break;
+//   case 6:
+//     console.log("Days in Month: 30");
+//     break;
+//   case 7:
+//     console.log("Days in Month: 31");
+//     break;
+//   case 8:
+//     console.log("Days in Month: 31");
+//     break;
+//   case 9:
+//     console.log("Days in Month: 30");
+//     break;
+//   case 10:
+//     console.log("Days in Month: 30");
+//     break;
+//   case 11:
+//     console.log("Days in Month: 30");
+//     break;
+//   case 12:
+//     console.log("Days in Month: 31");
+// }
+
+//    ***********************************************   16  *****************************
+
+// var char = window.prompt("enter word ");
+
+// switch(char)
+// {
+//     case 'A':
+//     case 'E':
+//     case 'I':
+//     case 'O':
+//     case 'U':
+//     case 'a':
+//     case 'e':
+//     case 'i':
+//     case 'o':
+//     case 'u':
+//         console.log("vowel");
+//         break;
+//     default:
+//         console.log("Consonant");
+
+// }
+
+//    ***********************************************   17  *****************************
+
+// var num1 = window.prompt("enter num1 ");
+// var num2 = window.prompt("enter num2 ");
+
+// switch (num1>num2) {
+//     case true:
+//         console.log(num1);
+//      break;
+//      case false:
+//          console.log(num2);
+//      break;
+//   }
+
+//    ***********************************************   18  *****************************
+
+// var num = window.prompt("enter num ");
+
+// switch (num % 2 == 0) {
+//   case true:
+//     console.log("even");
+//     break;
+
+//   default:
+//     console.log("odd");
+// }
+
+//    ***********************************************   19  *****************************
+
+// var num = window.prompt("enter num ");
+
+// switch (Math.sign(num)) {
+//   case 1:
+//     console.log("The number is Positive");
+//     break;
+//   case -1:
+//     console.log("The number is Negative");
+//     break;
+//   default:
+//     console.log("The number is Zero");
+// }
+
+//    ***********************************************   20  *****************************
+
+// var number1 =  Number (window.prompt("enter num1 "));
+// var operator = window.prompt("enter operator ");
+// var number2 =  Number (window.prompt("enter num2 "));
+
+// if (operator == "+") {
+//   console.log(number1 + number2);;
+// } else if (operator == "-") {
+//     console.log(number1 - number2);;
+// } else if (operator == "*") {
+//     console.log(number1 * number2);;
+// } else {
+//     console.log(number1 / number2);;
+// }
+
+
+
+
+
+
+
+
+var user = {
+    name:"ahmed",
+    age:22,
+    nymb:232332,
+    work:function() {
+console.log("work");
+
+    }
+
+}
